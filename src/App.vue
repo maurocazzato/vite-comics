0b629ba39bc9ead07c154343bf6e05d82062922a
Maurocazzato <maurizzio.125@gmail.com>
2023-12-22 14:35:30 +0100

    jumbotron

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,28 @@ export default {
 
 <template>
   <appHeader />
+  <main>
+    <div class="backgroundbox">
+      <!-- Contenuto del box qui -->
+    </div>
+  </main>
 </template>
 
 <style lang="scss">
 @use './styles/partials/_variables' as *;
 @use './styles/_general.scss' as *;
+
+.backgroundbox {
+  height: 300px;
+  background-image: url('public/jumbotron.jpg');
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.backgroundbox img {
+  max-width: 100%;
+  max-height: 100%;
+}
 </style>
